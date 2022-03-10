c80f07ffb01251e7f431046e8851581b2297cf79
Márton Elek <elek@apache.org>
2022-03-10 14:03:22 +0100

    web: allow to select basic duration

--- web/src/components/Pin.vue
+++ web/src/components/Pin.vue
@@ -11,13 +11,13 @@ var message = ref("")
 
 var tokenAddress = ""
 var pinAddress = ""
+var duration = ref(1)
 var allowance = ref(BigNumber.from(0))
 var balance = ref(BigNumber.from(0))
-var price = BigNumber.from("10000000")
+
 var defaultAllowance = BigNumber.from("10000000000")
 
 const provider = new ethers.providers.Web3Provider(window.ethereum)
-
 
 const tokenAbi = [
   "function transfer(address to, uint amount) returns (bool)",
@@ -29,8 +29,15 @@ var pinAbi = [
   "function pin(string cid,uint amount) returns ()"
 ]
 
+var price = function (d) {
+  if (d.value === 200) {
+    return BigNumber.from("2000000000")
+  } else {
+    return BigNumber.from(10000000).mul(d.value)
+  }
+}
 var allowed = function () {
-  return allowance.value.gt(price)
+  return allowance.value.gt(price(duration))
 }
 
 var checkAllowance = function () {
@@ -76,7 +83,7 @@ var pin = function () {
   }
   var signer = provider.getSigner(account.value)
   const contract = new ethers.Contract(pinAddress, pinAbi, signer)
-  contract.pin(cid.value, price).then(function (res) {
+  contract.pin(cid.value, price(duration)).then(function (res) {
   })
 }
 
@@ -91,8 +98,31 @@ var connect = function () {
 }
 
 var tokenStr = function (a) {
-  return a.div(1000000000)
+  var out = ""
+  var raw = a.toString()
+  var dec = 8
+  for (var i = raw.length - 1; i >= 0; i--) {
+    if (dec === 0 && out.length > 0) {
+      out = "." + out
+    }
+
+    if (out.length > 0 || dec <= 0 || raw[i] !== "0") {
+      out = raw[i] + out
+    }
+    dec--
+  }
+  if (dec >= 0 && out.length > 0) {
+    out = "0." + out
+  }
+  if (out.length == 0) {
+    out = "0"
+  }
+  return out
 }
+
+var p = computed(() => {
+  return price(duration)
+})
 </script>
 
 <template>
@@ -103,6 +133,19 @@ var tokenStr = function (a) {
 
     <input v-model="cid" type="text" id="inputAmount" class="mb-3 form-control" placeholder="IPFS hash"
            required autofocus>
+
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="duration" id="1y" value="1" v-model="duration">
+      <label class="form-check-label" for="1y">1 year</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="duration" id="2y" value="2" v-model="duration">
+      <label class="form-check-label" for="2y">2 years</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="duration" id="forever" value="200" v-model="duration">
+      <label class="form-check-label" for="forever">forever</label>
+    </div>
 
     <button v-if="!account" type="button"
             class="btn btn-lg btn-warning btn-block"
@@ -122,10 +165,13 @@ var tokenStr = function (a) {
       </button>
     </div>
 
-    <p class="mt-5 mb-3 text-muted">Pinning costs 0.1 STORJ + gas fee</p>
-    <p v-if="account">Address: {{ account }}</p>
-    <p v-if="balance">Balance: {{ tokenStr(balance)}}</p>
-    <p v-if="allowance">Allowance: {{ tokenStr(allowance) }}</p>
+
+    <p class="mt-5 mb-3 text-muted">Pinning costs {{ tokenStr(p) }} STORJ + gas fee</p>
+    <div v-if="account">
+      <p>Address: {{ account }}</p>
+      <p v-if="balance">Balance: {{ tokenStr(balance) }}</p>
+      <p v-if="allowance">Allowance: {{ tokenStr(allowance) }}</p>
+    </div>
   </form>
 </template>
 
