a7ed194f249792ddbd5ca6f142732c15c68df973
Márton Elek <elek@apache.org>
2022-03-10 10:20:20 +0100

    web: fix the approval workflow

--- web/src/components/Pin.vue
+++ web/src/components/Pin.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {computed, inject, onMounted, ref} from 'vue'
+import {computed, ref} from 'vue'
 import {BigNumber, ethers} from "ethers";
 import axios from "axios"
 
@@ -12,8 +12,9 @@ var message = ref("")
 var tokenAddress = ""
 var pinAddress = ""
 var allowance = ref(BigNumber.from(0))
+var balance = ref(BigNumber.from(0))
 var price = BigNumber.from("10000000")
-
+var defaultAllowance = BigNumber.from("10000000000")
 
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 
@@ -21,7 +22,8 @@ const provider = new ethers.providers.Web3Provider(window.ethereum)
 const tokenAbi = [
   "function transfer(address to, uint amount) returns (bool)",
   "function allowance(address owner, address spender) view returns (uint256)",
-  "function approve(address spender, uint256 amount) returns (bool)"];
+  "function approve(address spender, uint256 amount) returns (bool)",
+  "function balanceOf(address acc) view returns (uint256)"];
 
 var pinAbi = [
   "function pin(string cid,uint amount) returns ()"
@@ -39,12 +41,21 @@ var checkAllowance = function () {
   })
 }
 
+var checkBalance = function () {
+  var signer = provider.getSigner(account.value)
+  const contract = new ethers.Contract(tokenAddress, tokenAbi, signer)
+  contract.balanceOf(account.value).then(function (res) {
+    balance.value = res
+  })
+}
+
 var setAccount = function (a) {
   account.value = a;
   axios.get("/api/v0/pin/config").then(async function (config) {
     tokenAddress = config.data.TokenContract.replace(/0x/g, "");
     pinAddress = config.data.PinContract.replace(/0x/g, "");
     checkAllowance()
+    checkBalance()
   })
 }
 
@@ -52,7 +63,7 @@ var setAccount = function (a) {
 var approve = function () {
   var signer = provider.getSigner(account.value)
   const contract = new ethers.Contract(tokenAddress, tokenAbi, signer)
-  contract.approve(account.value, pinAddress).then(function (res) {
+  contract.approve(pinAddress, defaultAllowance).then(function (res) {
     checkAllowance()
   })
 }
@@ -79,12 +90,14 @@ var connect = function () {
   })
 }
 
-
+var tokenStr = function (a) {
+  return a.div(1000000000)
+}
 </script>
 
 <template>
   <form class="form-signin">
-    <h1 class="h3 mb-3 font-weight-normal">Pin your IPFS hash {{ allowance }} </h1>
+    <h1 class="h3 mb-3 font-weight-normal">Pin your IPFS hash</h1>
 
     <p>{{ message }}</p>
 
@@ -109,7 +122,10 @@ var connect = function () {
       </button>
     </div>
 
-    <p class="mt-5 mb-3 text-muted">Pinning costs n STORJ tokens / megabytes / days</p>
+    <p class="mt-5 mb-3 text-muted">Pinning costs 0.1 STORJ + gas fee</p>
+    <p v-if="account">Address: {{ account }}</p>
+    <p v-if="balance">Balance: {{ tokenStr(balance)}}</p>
+    <p v-if="allowance">Allowance: {{ tokenStr(allowance) }}</p>
   </form>
 </template>
 
