a30b4cf59e681f11b0181f39d644474dcb19f90e
Márton Elek <elek@apache.org>
2022-03-13 12:04:32 +0100

    web: page to show submitted pin transactions

---
+++ web/src/components/Tx.vue
@@ -0,0 +1,38 @@
+<script setup>
+import axios from "axios";
+import {computed, ref} from 'vue'
+
+var transactions = ref()
+axios.get("/api/v0/pin/all").then(async function (res) {
+  transactions.value = res.data
+})
+</script>
+
+<template>
+  <div>
+
+    <main role="main">
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col">Transaction</th>
+          <th scope="col">Index</th>
+          <th scope="col">Cid</th>
+          <th scope="col">Amount</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="tx in transactions">
+          <td>{{ tx.Transaction }}</td>
+          <td>Otto</td>
+          <td>{{ tx.Cid }}</td>
+          <td>{{ tx.TokenValue }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- web/src/main.js
+++ web/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import {createRouter, createWebHistory} from 'vue-router'
 import Pin from "./components/Pin.vue";
 import Home from "./components/Home.vue";
+import Tx from "./components/Tx.vue";
 
 
 const routes = [
     { path: '/', component: Home },
     { path: '/pin', component: Pin },
+    { path: '/tx', component: Tx },
 ]
 
 
