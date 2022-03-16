8f3bef304dbccff420a35b3aecaa1947d3145db7
Márton Elek <elek@apache.org>
2022-03-16 11:33:53 +0100

    web: move the pinning form to the landing page

--- web/src/main.js
+++ web/src/main.js
@@ -1,16 +1,17 @@
 import {createApp} from 'vue/dist/vue.esm-bundler.js'
 import {createRouter, createWebHistory} from 'vue-router'
 import Pin from "./components/Pin.vue";
-import Home from "./components/Home.vue";
+import Upload from "./components/Upload.vue";
 import Tx from "./components/Tx.vue";
 import Block from "./components/Block.vue";
 import Blocks from "./components/Blocks.vue";
 
 
 const routes = [
-    {path: '/', component: Home},
+    {path: '/', component: Pin},
+    {path: '/pin/', component: Pin},
     {path: '/pin/:hash', component: Pin, props: true},
-    {path: '/pin', component: Pin},
+    {path: '/upload', component: Upload},
     {path: '/tx', component: Tx},
     {path: '/block/:hash', component: Block, props: true},
     {path: '/blocks', component: Blocks},
