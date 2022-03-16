806c9b1edcb32f6eee4d194562b7836bc9584e2c
Márton Elek <elek@apache.org>
2022-03-16 18:31:32 +0100

    web: switch back to web hash history

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,5 @@
 import {createApp} from 'vue/dist/vue.esm-bundler.js'
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Pin from "./components/Pin.vue";
 import Upload from "./components/Upload.vue";
 import Tx from "./components/Tx.vue";
@@ -19,7 +19,7 @@ const routes = [
 
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
